e4076be10375fec062da650fed8681a02e0d2e65
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-31 05:50:11 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -11,4 +11,4 @@ end
 
 petrol_petes = GasStation.new("Petrol Pete's", 50)
 seashore_shell = GasStation.new("The Seashore Shell", 40)
-dinobones = GasStation.new("Dino Bones Gas and Grill", 100)
+dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
