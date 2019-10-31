bb2e4657a2ed0f220570ad4aefd48c8dbbf3a10b
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-31 06:02:45 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
-binding.pry
+
 def start_game(player1, player2)
   "Hello #{player1} & #{playar2}"
 end
-
+binding.pry
 def play_game(player1, player2)
   "#{player1} is better than #{player2}"
 end
